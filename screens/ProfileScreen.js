d7d0f5c7b0c06f39e8687b97e0b31c64a94d1187
OskinB <osk.bjorns90@gmail.com>
2021-06-11 23:52:23 +0200

    Initial

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,29 +1,62 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet, } from 'react-native';
-import { DarkPurple, Purple } from '../assets/colors';
-import { useSelector } from 'react-redux';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { View, Text, Button, StyleSheet, Image } from "react-native";
+import { DarkPurple, Pink, Purple } from "../assets/colors";
+import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
-const ProfileScreen = props => {
-    const navigation = useNavigation()
-    const userInfo = useSelector(state => state.user.loggedInUser)
-    console.log("Profile name: " + userInfo.name);
+const ProfileScreen = (props) => {
+	const navigation = useNavigation();
+	const userInfo = useSelector((state) => state.user.loggedInUser);
+	console.log("Profile name: " + userInfo.name);
 
-    return (
-        <View>
-            <View>
-                <Text style={styles.text}>{userInfo.name}</Text>
-                <Text style={styles.text}>{userInfo.email}</Text>
-            </View>
-            <Button title="Edit profile" onPress={() => navigation.navigate("Edit Profile")}></Button>
-        </View>
-    );
-}
+	// <Text style={styles.text}>{userInfo.name || "Your name"}</Text>
+	// <Text style={styles.text}>{userInfo.email || "Your email"}</Text>
+
+	return (
+		<View>
+			<View style={styles.userProfile}>
+				<Image
+					style={styles.userImage}
+					source={require("../assets/adaptive-icon.png")}
+				/>
+				<View style={styles.userDetails}>
+					<Text style={styles.detailsName}>Name</Text>
+					<Text style={styles.detailsText}>Email</Text>
+					<Text style={styles.detailsText}>Title</Text>
+				</View>
+			</View>
+			<Button
+				title="Edit profile"
+				onPress={() => navigation.navigate("Edit Profile")}
+			></Button>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-    text: {
-        color: DarkPurple,
-    }
+	userProfile: {
+		flexDirection: "row",
+		marginVertical: 20,
+		marginHorizontal: 10,
+	},
+	userImage: {
+		backgroundColor: Pink,
+		height: 60,
+		width: 60,
+		borderRadius: 50,
+		margin: 10,
+	},
+	userDetails: {
+		margin: 10,
+	},
+	detailsName: {
+		color: "#5050a5",
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+	detailsText: {
+		fontSize: 16,
+	},
 });
 
 export default ProfileScreen;
